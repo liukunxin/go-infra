5fb49f9e28e2f31c2c5c3014bf9da10c152043ee
kunxin <liu_kunxin@163.com>
2025-12-04 22:55:32 +0800

    加注释

--- pkg/log/config.go
+++ pkg/log/config.go
@@ -2,8 +2,8 @@ package log
 
 // Config 配置
 type Config struct {
-	Level      int
-	Formatter  string // "text" 或 "json"
-	Output     string // "stdout"（可扩展 file/kafka）
-	BufferSize int    // 异步缓冲区大小，默认 1000
+	Level      int    `json:"level" toml:"level" yaml:"level"`
+	Formatter  string `json:"formatter" toml:"formatter" yaml:"formatter"`       // "text" 或 "json"
+	Output     string `yaml:"output" json:"output" toml:"output"`                // "stdout"（可扩展 file/kafka）
+	BufferSize int    `json:"buffer_size" yaml:"buffer_size" toml:"buffer_size"` // 异步缓冲区大小，默认 1000
 }
--- pkg/trace/config.go
+++ pkg/trace/config.go
@@ -1,6 +1,6 @@
 package trace
 
 type Config struct {
-	ServiceName *string  `yaml:"serviceName,omitempty" json:"serviceName,omitempty" toml:"serviceName,omitempty"`
-	SampleRatio *float64 `yaml:"sampleRatio,omitempty" json:"sampleRatio,omitempty" toml:"sampleRatio,omitempty"`
+	ServiceName *string  `yaml:"service_name,omitempty" json:"service_name,omitempty" toml:"service_name,omitempty"`
+	SampleRatio *float64 `yaml:"sample_ratio,omitempty" json:"sample_ratio,omitempty" toml:"sample_ratio,omitempty"`
 }
